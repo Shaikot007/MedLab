223220da02d385d935afce2b23a3f5203b20f31f
Shaikot007 <shaikot999@gmail.com>
2022-10-12 21:36:23 +0600

    medlab client lab list modified

--- medlab-client/src/Components/LabList/LabList.jsx
+++ medlab-client/src/Components/LabList/LabList.jsx
@@ -5,9 +5,8 @@ import Facility from '../Facilities/Facilities';
 import Footer from '../Footer/Footer';
 import { Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import LabImg from '../../Images/Lab/lab.png';
+import LabImg from '../../Images/Lab/lab.png';
 import {
-  faDiagnoses,
   faCertificate,
   faLocationPin,
   faArrowRight
@@ -24,58 +23,52 @@ function LabList() {
               <h4>Certified Labs</h4>
               <h5>2 Tests</h5>
             </div>
+            <div className="LabNameHeadline">
+              <FontAwesomeIcon icon={faCertificate} size='sm' style={{ color: 'orange' }} />
+              <span style={{ paddingLeft: '5px' }}>Reliabilty assured with Certified Labs</span>
+            </div>
             <div className="LabCard">
+              <div className="BlankBox">
+                <img src={LabImg} alt="LabImage" style={{ height: 'auto', width: '100%' }} />
+              </div>
               <div className="LabCardDetail">
                 <div className="LabName">
-                  <FontAwesomeIcon icon={faDiagnoses} size='lg' style={{ padding: '15px 5px 15px 5px', color: '10847e' }} />
                   <h5>PharmEasy Labs</h5>
+                  <FontAwesomeIcon icon={faArrowRight} size='lg' className="LeftArrow" style={{ color: '#10847e' }} />
                 </div>
                 <div className="LabName">
-                  <div className="BlankBox">
-                  </div>
-                  <p style={{ color: 'grey' }}>
+                  <p style={{ color: 'grey', paddingLeft: '10px' }}>
                     <FontAwesomeIcon icon={faCertificate} size='sm' /> NABL, CAP Certified
                   </p>
                 </div>
                 <div className="LabName">
-                  <div className="BlankBox">
-                  </div>
-                  <p style={{ color: 'grey' }}>
+                  <p style={{ color: 'grey', paddingLeft: '10px' }}>
                     <FontAwesomeIcon icon={faLocationPin} size='sm' /> Dhaka, Bangladesh
                   </p>
                 </div>
               </div>
-              <div className="LabCardButton">
-                <FontAwesomeIcon icon={faArrowRight} size='lg' className="LeftArrow" />
-              </div>
             </div>
-
             <div className="LabCard">
+              <div className="BlankBox">
+                <img src={LabImg} alt="LabImage" style={{ height: 'auto', width: '100%' }} />
+              </div>
               <div className="LabCardDetail">
                 <div className="LabName">
-                  <FontAwesomeIcon icon={faDiagnoses} size='lg' style={{ padding: '15px 5px 15px 5px', color: '10847e' }} />
                   <h5>PharmEasy Labs</h5>
+                  <FontAwesomeIcon icon={faArrowRight} size='lg' className="LeftArrow" style={{ color: '#10847e' }} />
                 </div>
                 <div className="LabName">
-                  <div className="BlankBox">
-                  </div>
-                  <p style={{ color: 'grey' }}>
+                  <p style={{ color: 'grey', paddingLeft: '10px' }}>
                     <FontAwesomeIcon icon={faCertificate} size='sm' /> NABL, CAP Certified
                   </p>
                 </div>
                 <div className="LabName">
-                  <div className="BlankBox">
-                  </div>
-                  <p style={{ color: 'grey' }}>
-                    <FontAwesomeIcon icon={faLocationPin} size='sm' /> Chottogram, Bangladesh
+                  <p style={{ color: 'grey', paddingLeft: '10px' }}>
+                    <FontAwesomeIcon icon={faLocationPin} size='sm' /> Dhaka, Bangladesh
                   </p>
                 </div>
               </div>
-              <div className="LabCardButton">
-                <FontAwesomeIcon icon={faArrowRight} size='lg' className="LeftArrow" />
-              </div>
             </div>
-
           </div>
         </div>
         <div className="CartBox">
