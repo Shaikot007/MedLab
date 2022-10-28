b8d2d648e0c7610194d9c206ad54586e9aa27d58
Shaikot007 <shaikot999@gmail.com>
2022-10-28 18:51:06 +0600

    medlab server dashboard modify

--- medlab-server/resources/views/admin/includes/sidebar.blade.php
+++ medlab-server/resources/views/admin/includes/sidebar.blade.php
@@ -2,22 +2,42 @@
 <nav class="page-sidebar" id="sidebar">
     <div id="sidebar-collapse">
         <div class="admin-block d-flex">
-            <div class="admin-info">
-                <div class="font-strong">Admin panel</div>
-                <small>Administrator</small>
-            </div>
         </div>
         <ul class="side-menu metismenu">
             <li class="heading">FEATURES</li>
             <li>
-                <a href="javascript:;"><i class="sidebar-item-icon fa fa-th-large"></i>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-hospital-o"></i>
+                    <span class="nav-label">Laboratory</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="#">Add Laboratory</a>
+                    </li>
+                    <li>
+                        <a href="#">Manage Laboratory</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-sticky-note-o"></i>
                     <span class="nav-label">Test</span><i class="fa fa-angle-left arrow"></i></a>
                 <ul class="nav-2-level collapse">
                     <li>
-                        <a href="#">Add test</a>
+                        <a href="#">Add Test</a>
                     </li>
                     <li>
-                        <a href="#">Manage test</a>
+                        <a href="#">Manage Test</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;"><i class="sidebar-item-icon fa fa-users"></i>
+                    <span class="nav-label">Users</span><i class="fa fa-angle-left arrow"></i></a>
+                <ul class="nav-2-level collapse">
+                    <li>
+                        <a href="#">Add User</a>
+                    </li>
+                    <li>
+                        <a href="#">Manage User</a>
                     </li>
                 </ul>
             </li>
